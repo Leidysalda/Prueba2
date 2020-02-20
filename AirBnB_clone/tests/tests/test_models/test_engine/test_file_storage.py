b63374c35aa553230373c240f273711e4573df6b
Leidysalda <leidysalda1@gmail.com>
2020-02-20 22:34:38 +0000

    Add tests

---
+++ AirBnB_clone/tests/tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Test FileStorage"""
+import unittest
+from models.base_model import BaseModel
+
+class test_FileStorage(unittest.TestCase):
+    """ Test FileStorage """
+
+    def test_1(self):
+        pass
